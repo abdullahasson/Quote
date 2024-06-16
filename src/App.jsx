d32d46d4639d6d1608773e5749ad8d206361797c
abdullahasson <bh532997@gmail.com>
2024-06-16 17:16:29 +0300

    add like sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import axios from "axios";
 import React from "react";
+import Loader from "./components/loaders/Loader";
 import Speacker from "./assets/Speacker";
 import Copy from "./assets/Copy";
 import Done from "./assets/Done";
+import Save from "./assets/Save"
+import Saved from "./assets/Saved"
+
 
 class App extends React.Component {
 
-  state = {advice : '' , copied: false , isLoading: false}
+  state = {advice : '', adviceID: '' ,  copied: false , isLoading: false , isSaved: false}
 
   componentDidMount() {
     this.fetchAdvice();
+  }
+
+  checkSaved = (parm) => {
+    window.localStorage.getItem(parm) ? true : false
   }
 
   fetchAdvice = async () => {
@@ -17,7 +25,11 @@ class App extends React.Component {
     try {
       const response = await axios.get('https://api.adviceslip.com/advice');
       const advice = response.data.slip.advice;
-      this.setState({ advice });
+      const id = response.data.slip.id
+      this.setState({ advice , adviceID : id});
+
+      this.checkSaved(id) ? this.setState({ isSaved : true }) : this.setState({ isSaved : false })
+
     } catch (error) {
       console.log(error);
     }
@@ -36,17 +48,28 @@ class App extends React.Component {
     this.setState({ copied : true })
   }
 
+  saveAdvice = () => {
+
+    const key = this.state.adviceID
+    const value = this.state.advice
+
+    this.checkSaved(key) ? 
+      this.setState({ isSaved : true}) :
+      window.localStorage.setItem(key , value)
+      this.setState({ isSaved : true})
+
+  }
+
   render() {
-    const {advice , copied , isLoading} = this.state
+    const {advice , copied , isLoading , isSaved } = this.state
     return (
       <div className="app">
 
         <div className="card">
           <h1 className="heading">
-            {isLoading ? <>Loading...</> : <q>{advice}</q>}
+            {isLoading ? <Loader/> : <q>{advice}</q>}
           </h1>
 
-          
           <button className="button" onClick={this.fetchAdvice}>
             <span>GIVE ME ADVICE!</span>
           </button>
@@ -57,8 +80,11 @@ class App extends React.Component {
 
           <button style={{display : isLoading && "none"}} className="copy_button" onClick={this.copyAdvice}>
               {copied ? <Done/> : <Copy/>}
-            </button>
+          </button>
 
+          <button style={{display : isLoading && "none"}} className="save_button" onClick={this.saveAdvice}>
+              {isSaved ? <Saved/> : <Save/>}
+          </button>
         </div>
       </div>
     )
---
+++ src/components/loaders/Loader.jsx
@@ -0,0 +1,9 @@
+import "./Loader.css"
+
+const Loader = () => (
+  <svg className="svg" viewBox="25 25 50 50">
+    <circle r={20} cy={50} cx={50} />
+  </svg>
+)
+
+export default Loader
